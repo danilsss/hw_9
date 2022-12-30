44d1bc7af34d092954c0294c39c4284d0075ccfe
danil <sdy2001@yandex.ru>
2022-12-30 16:47:20 +0300

    added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+*/
+
+Console.WriteLine("Введите число больше 1:");
+int number = int.Parse(Console.ReadLine());
+
+void NumberCounter (int number)
+{
+if (number < 0) Console.Write($"{number} не натуральное число");
+if (number == 0) return;
+Console.Write("{0,4}", number);
+NumberCounter (number - 1);
+}
+
+NumberCounter(number);
+
+
+/*Задача: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+*/
+
+Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
